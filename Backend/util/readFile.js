f7868ceb474a2211ff1804f47c3ec66fa924dc0d
ChamroeunProMaxQ <stevenkakoreal@gmail.com>
2023-07-19 16:52:57 +0700

    docker

--- Backend/util/readFile.js
+++ Backend/util/readFile.js
@@ -1,7 +1,8 @@
 const fs = require("fs");
+const path = require("path");
 
 const readFile = () => {
-    const data = fs.readFileSync("data.json",'utf-8');
+    const data = fs.readFileSync(path.resolve('data.json'),'utf-8');
     const dataJSON = JSON.parse(data);
     return dataJSON;
 }
--- Backend/util/writeFile.js
+++ Backend/util/writeFile.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 
+
 const writeFile = (data) => {
    const res = fs.writeFileSync("data.json", JSON.stringify(data));
    console.log(res)
